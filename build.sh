cfadf9cb15dc461cd20eaadf3c18e1f1c670efb1
Andreas Stokholm <andreas@stokholm.me>
2023-01-03 22:25:17 +0100

    include installation of dependencies in build script

--- build.sh
+++ build.sh
@@ -4,10 +4,12 @@ git clone git@github.com:t1dtools/wrapped.git wrapped
 
 # Build wrapped
 cd wrapped
+npm install
 npm run build
 
 # Build web
 cd ..
+npm install
 npm run build
 
 # Copy wrapped to web
