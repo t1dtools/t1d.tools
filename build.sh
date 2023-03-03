a857e1a64b1afaca1e5f8185daece05a9437cd6c
Andreas Stokholm <andreas@stokholm.me>
2023-03-03 16:33:30 +0100

    add dash-basal site

--- build.sh
+++ build.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
 git clone git@github.com:t1dtools/wrapped.git wrapped
+git clone git@github.com:t1dtools/dash-basal.git dash-basal
 
 # Build wrapped
 cd wrapped
+npm install
+npm run build
+
+# Build dash-basal
+cd dash-basal
 npm install
 npm run build
 
@@ -15,3 +21,7 @@ npm run build
 # Copy wrapped to web
 mkdir -p ./dist/wrapped
 cp -r ./wrapped/out/* ./dist/wrapped/
+
+# Copy dash-basal to web
+mkdir -p ./dist/dash-basal
+cp -r ./dash-basal/out/* ./dist/dash-basal/
